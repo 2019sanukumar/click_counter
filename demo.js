72fbcad2470a311679b8b6b0a420d45322ce0af0
Sanu Kumar <sanukumar@Sanus-MacBook-Air.local>
2021-07-13 09:10:09 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,31 @@
+// console.log(window);
+var sanu=document.getElementById('one');
+sanu.style.color="cyan";
+sanu.style.backgroundColor="red";
+
+sanu.addEventListener('click',function(){
+    sanu.innerText="you clicked here right !";
+    sanu.style.backgroundColor="blue";
+
+});
+var button=document.getElementById("b");
+
+var count=0;
+var print=document.getElementById("print");
+button.addEventListener('click',function(){
+    button.innerHTML="you clicked";
+    count++;
+    print.innerHTML=count;
+
+    // window.alert('he clicked');
+
+
+});
+
+// for any key press
+
+    // document.addEventListener('keydown',function(event)
+    // {
+    //     console.log('Keydown',event.keyCode);
+
+    // });
